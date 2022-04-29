dfdfd76991d9880d7345a91620dd682103ec68ee
Eric Wilson <ewpiano@gmail.com>
2022-04-28 20:54:35 -0500

    Added logo and updated main.css.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const express = require('express');
+const fs = require('fs');
+
 const app = express();
 const port = 3000;
 
@@ -11,7 +13,7 @@ app.set('view engine', 'ejs');
 // Routes
 app.get('/', (req, res) => {
     res.render('index', {
-        title: 'Random Quote Generator',
+        title: 'MOMENTO',
         page: 'Home',
         menuId: 'home'
     })
@@ -21,3 +23,18 @@ app.listen(port, (err) => {
     if(err) throw err
     console.log(`App listening on port: ${port}`);
 });
+
+// Load jsonReader() Helper Function
+function jsonReader(filePath, cb) {
+    fs.readFile(filePath, (err, fileData) => {
+        if (err) {
+            return cb && cb(err);
+        }
+        try {
+            const object = JSON.parse(fileData);
+            return cb && cb(null, object);
+        } catch(err) {
+            return cb && cb(err);
+        }
+    });
+}
